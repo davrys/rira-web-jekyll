a028d8eb55d6c267d1f2fd09031d280c9c02e4be
David Ryšánek <david@rysanek.pro>
2022-04-15 00:07:08 +0200

    Jekyll docker container 3.2.1

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,5 @@
 # Run the docker image (and download all dependencies)
 # https://docs.docker.com/compose/overview/
-docker compose up "site" --remove-orphans
+docker compose up "site"
+# docker compose up "site" --remove-orphans
 # docker compose up
